3f60094dd30426eec74ba01e9a70fd9e3f7f31ab
Battilus <battilus@hotmail.com>
2022-01-17 23:25:11 +0300

    refactored GroupControl and added defaul value

--- src/components/GroupsWrapper/GroupsWrapper.jsx
+++ src/components/GroupsWrapper/GroupsWrapper.jsx
@@ -1,155 +1,86 @@
 import React, {useState} from "react";
-import {nanoid} from "nanoid";
 import GroupControl from "./GroupControl/GroupControl";
 
 
-const GroupsWrapper = (props) => {
-    const initialGroups = [
-        {
-            id: 1,
-            label: 'Test 1',
-        },
-        {
-            id: 2,
-            label: 'Test 2',
-        },
-        {
-            id: 3,
-            label: 'Test 3',
-        },
-        {
-            id: 4,
-            label: 'Test 4',
-        },
-        {
-            id: 5,
-            label: 'Test 5',
-        },
-        {
-            id: 6,
-            label: 'Test 6',
-        },
-        {
-            id: 7,
-            label: 'Test 7',
-        },
-        {
-            id: 8,
-            label: 'Test 8',
-        },
-        {
-            id: 9,
-            label: 'Test 9',
-        },
-        {
-            id: 10,
-            label: 'Test 10',
-        },
-        {
-            id: 11,
-            label: 'Test 11',
-        },
-        {
-            id: 12,
-            label: 'Test 12',
-        },
-        {
-            id: 13,
-            label: 'Test 13',
-        },
-        {
-            id: 14,
-            label: 'Test 14',
-        },
-        {
-            id: 15,
-            label: 'Test 15',
-        },
-        {
-            id: 16,
-            label: 'Test 16',
-        },
-        {
-            id: 17,
-            label: 'Test 17',
-        },
-        {
-            id: 18,
-            label: 'Test 18',
-        },
-        {
-            id: 19,
-            label: 'Test 19',
-        },
-        {
-            id: 20,
-            label: 'Test 20',
-        },
-    ] || props.values
+const GroupsWrapper = ({values, ...props}) => {
+    const initialGroups = values ||
+        [
+            "Test 1",
+            "Test 2",
+            "Test 3",
+            "Test 4",
+            "Test 5",
+            "Test 6",
+            "Test 7",
+            "Test 8",
+            "Test 9"
+        ];
 
     const [store, setStore] = useState({
-        selected: {
-            id: '',
-            label: ''
-        },
-        groups: initialGroups
-    })
+        selected: initialGroups[0],
+        groups: initialGroups,
+        newGroupVal: "",
+    });
 
     const groupReducer = (action, payload) => {
         switch (action) {
-            case 'addGroup':
-                const updatedGroups = store.groups
-                updatedGroups.push({
-                    id: nanoid(),
-                    label: payload
-                })
-                setStore({...store, groups: updatedGroups})
-                break
 
-            case 'deleteGroup':
-                const clearedGroups = store.groups.filter(item => item.id !== payload)
+            case "updateNewGroupVal":
+                if (store.newGroupVal.length < 32) setStore({...store, newGroupVal: payload});
+                break;
+
+            case "addGroup":
+                if (!store.groups.find((item) => item === store.newGroupVal)) {
+                    const updatedGroup = store.groups;
+                    const newGroup = store.newGroupVal;
+                    updatedGroup.push(newGroup);
+                    setStore({
+                        ...store,
+                        selected: newGroup,
+                        groups: updatedGroup,
+                        newGroupVal: ""
+                    });
+                }
+                break;
+
+            case "deleteGroup":
+                const clearedGroups = store.groups.filter((item) => item !== payload);
                 setStore({
                     ...store,
-                    selected: {
-                        id: '',
-                        label: ''
-                    },
-                    groups: clearedGroups
-                })
-                break
+                    selected: "",
+                    groups: clearedGroups,
+                });
+                break;
 
-            case 'selectGroup':
+            case "selectGroup":
                 setStore({
                     ...store,
-                    selected: {
-                        id: payload.id,
-                        label: payload.label
-                    }
-                })
-                break
+                    selected: payload,
+                });
+                break;
 
-            case 'clearSelector':
+            case "clearSelector":
                 setStore({
                     ...store,
-                    selected: {
-                        id: '',
-                        label: ''
-                    }
-                })
-                break
+                    selected: "",
+                });
+                break;
 
             default:
-                setStore(store)
+                setStore(store);
         }
-    }
-
+    };
 
     return (
-        <div className='container'>
-            <GroupControl label={store.selected.label} values={store.groups} selected={store.selected.id}
-                          reducer={groupReducer}/>
+        <div className="container">
+            <GroupControl
+                values={store.groups}
+                selected={store.selected}
+                newGroupName={store.newGroupVal}
+                reducer={groupReducer}
+            />
         </div>
     );
-}
+};
 
 export default GroupsWrapper;
